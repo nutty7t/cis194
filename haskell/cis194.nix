9825f21766163145ccace5e6fada261327fe1744
nutty7t <blackhole@nutty.email>
2020-08-28 11:56:47 -0700

    [HS] Add `hspec` dependency

--- haskell/cis194.nix
+++ haskell/cis194.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, base, hspec, stdenv }:
 mkDerivation {
   pname = "cis194";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base ];
+  executableHaskellDepends = [ base hspec ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
