3244131d6e1375553786c945f9c25ce5897d9183
nutty7t <blackhole@nutty.email>
2020-08-28 16:04:43 -0700

    [HS] Add `safe` dependency

--- haskell/cis194.nix
+++ haskell/cis194.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, hspec, stdenv }:
+{ mkDerivation, base, hspec, safe, stdenv }:
 mkDerivation {
   pname = "cis194";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base hspec ];
+  executableHaskellDepends = [ base hspec safe ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
