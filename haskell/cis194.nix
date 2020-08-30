52c2a2109b6bd340950856968c9e020dc6b2f391
nutty7t <blackhole@nutty.email>
2020-08-30 12:27:13 -0700

    [HS] Add `string-qq` dependency

--- haskell/cis194.nix
+++ haskell/cis194.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, hspec, safe, stdenv }:
+{ mkDerivation, base, hspec, safe, stdenv, string-qq }:
 mkDerivation {
   pname = "cis194";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base hspec safe ];
+  executableHaskellDepends = [ base hspec safe string-qq ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
