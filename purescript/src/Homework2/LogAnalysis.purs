954ddd25eeeb7a967f3c2fc74cf72d69f0bc3a36
nutty7t <blackhole@nutty.email>
2020-09-01 20:11:58 -0700

    [PS] Complete Homework 2 exercises

--- purescript/src/Homework2/LogAnalysis.purs
+++ purescript/src/Homework2/LogAnalysis.purs
@@ -2,12 +2,12 @@ module Homework2.LogAnalysis where
 
 import Prelude
 import Control.Monad.State (State, get, modify, put, runState)
-import Data.Array (drop, head, intercalate)
+import Data.Array (drop, filter, foldr, head, intercalate)
 import Data.Int (fromString)
 import Data.Maybe (Maybe(..))
 import Data.String.Utils (lines, words)
 import Data.Tuple (Tuple(..))
-import Homework2.Log (LogMessage(..), MessageType(..))
+import Homework2.Log (LogMessage(..), MessageTree(..), MessageType(..))
 
 --
 -- Exercise 1
@@ -63,3 +63,51 @@ parseMessage m = case runState parseMessageType $ lex m of
 
 parse :: String -> Array LogMessage
 parse = map parseMessage <<< lines
+
+--
+-- Exercise 2
+--
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) t = t
+insert m@(LogMessage _ _ _) Leaf = Node Leaf m Leaf
+insert m@(LogMessage _ ts _) t@(Node left m' right) = case m' of
+  Unknown _ -> t
+  (LogMessage _ ts' _) ->
+    if ts <= ts' then
+      (Node (insert m left) m' right)
+    else
+      (Node left m' (insert m right))
+
+--
+-- Exercise 3
+--
+
+build :: Array LogMessage -> MessageTree
+build messages = foldr insert Leaf messages
+
+--
+-- Exercise 4
+--
+
+inOrder :: MessageTree -> Array LogMessage
+inOrder Leaf = []
+inOrder (Node l m r) = inOrder l <> [ m ] <> inOrder r
+
+--
+-- Exercise 5
+--
+
+sortMessages :: Array LogMessage -> Array LogMessage
+sortMessages = inOrder <<< build
+
+getMessage :: LogMessage -> String
+getMessage (Unknown m) = m
+getMessage (LogMessage _ _ m) = m
+
+whatWentWrong :: Array LogMessage -> Array String
+whatWentWrong = map getMessage <<< sortMessages <<< filter isSevere
+  where
+  isSevere :: LogMessage -> Boolean
+  isSevere (LogMessage (Error s) _ _) = s >= 50
+  isSevere _ = false
